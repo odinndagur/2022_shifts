c5a2c2198f27025be553f39db4fe41742c89a4f9
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2022-10-02 09:38:24 +0000

    lol

--- python/word_dagplan/main.py
+++ python/word_dagplan/main.py
@@ -1,36 +1,44 @@
 import docx
 import pandas as pd
-
 import re
-
-doc = docx.Document('manlol.docx')
-
-df = pd.read_csv('/Users/odinndagur/Downloads/vaktaplans_test/Vaktaplan 11.07-10.08 lokaútgáfa.csv')
-
+import os
+from weekday import get_weekday
 from collections import defaultdict
-day = defaultdict(lambda: defaultdict(list))
 
+doc = docx.Document('fim_proto.docx')
+
+
+input_filename = 'Vaktaplan 11.07-10.08 lokaútgáfa.csv'
+df = pd.read_csv(input_filename)
+df.index = df[df.columns[0]]
+df.drop('Starfsmaður',axis=1)
+
+day = defaultdict(lambda: defaultdict(list))
+date = df.columns[1]
 for person,shift in df.iloc[:,0:2].dropna().values:
     if shift != 'ORLOF':
         time, type = shift.split(' ')
-        starttime,endtime = time.split('-')
-        starthr = int(starttime.split(':')[0])
-        endhr = int(endtime.split(':')[0])
-        if 0 < starthr < 12:
-            if endhr > 16:
+        if type == 'UB':
+            day['UB']['ub'].append(' '.join([person,time]))
+        else:
+            starttime,endtime = time.split('-')
+            starthr = int(starttime.split(':')[0])
+            endhr = int(endtime.split(':')[0])
+            if 0 < starthr < 12:
+                if endhr > 16:
+                    col = 'dv'
+                else:
+                    col = 'mv'
+            if 12 <= starthr < 16:
                 col = 'dv'
-            else:
-                col = 'mv'
-        if 12 <= starthr < 16:
-            col = 'dv'
-        if 16 <= starthr < 23:
-            col = 'kv'
-        if 23 <= starthr <= 24:
-            col = 'nv'
-        if 20 < endhr <= 24:
-            col = 'kv'
-        print(person,starthr,endhr,type,col)
-        day[type][col].append(','.join([person,time,type,col]))
+            if 16 <= starthr < 23:
+                col = 'kv'
+            if 23 <= starthr <= 24:
+                col = 'nv'
+            if 20 < endhr <= 24:
+                col = 'kv'
+            print(person,starthr,endhr,type,col)
+            day[type][col].append(','.join([person,time,type,col]))
 
 
 # for col_idx,col in enumerate(doc.tables[1].columns):
@@ -53,6 +61,21 @@ for person,shift in df.iloc[:,0:2].dropna().values:
             
 for col_idx,col in enumerate(doc.tables[1].columns):
     for cell in doc.tables[1].column_cells(col_idx):
+        if cell.text.strip() == 'dags':
+            p = cell.paragraphs[0]
+            p.clear()
+            p.add_run(date.split('\n')[0])
+        if cell.text.strip() == 'UB':
+            p = cell.paragraphs[0]
+            p.clear()
+            p.add_run('\n'.join(day['UB']['ub']))
+        if cell.text.strip() == 'vikudags':
+            p = cell.paragraphs[0]
+            p.clear()
+            decimal_date = date.split('\n')[0]
+            date_day, date_month = decimal_date.split('.')
+            weekday = get_weekday(month = date_month, day = date_day)
+            p.add_run(weekday)
         if re.match('\D{2,3} [a-z]{2}',cell.text.strip()):
             shift = cell.text.split(' ')
             if not shift[0] in day:
@@ -60,10 +83,17 @@ for col_idx,col in enumerate(doc.tables[1].columns):
             else:
                 # print(cell.text)
                 if shift[1] in day[shift[0]]:
-                    ppl = [' '.join(p.split(',')[:2]) for p in day[shift[0]][shift[1]]]
-                    ppl = sorted(ppl, key=lambda x: int(x.split(' ')[-1].split(':')[0]))
+                    # ppl = [' '.join(p.split(',')[:2]) for p in day[shift[0]][shift[1]]]
+                    temp = ' ' if 'NV' in shift[0] else '\n'
+                    ppl = [
+                        p.split(',')[0].split(' ')[0] + temp + p.split(',')[1] for p in day[shift[0]][shift[1]]
+                    ]
+                    ppl = sorted(ppl, key=lambda x: int(x.split(temp)[-1].split(':')[0]))
                     p = cell.paragraphs[0]
                     p.clear()
+
+                    # for p in ppl:
+
                     p.add_run('\n'.join(ppl))
                     # cell.text.
                     # print(cell.text,'\n\t',day[shift[0]][shift[1]])
@@ -72,7 +102,14 @@ for col_idx,col in enumerate(doc.tables[1].columns):
             # else:
             #     cell.paragraphs[0].clear()
 
-doc.save('/Users/odinndagur/Desktop/editad.docx')
+for col_idx,col in enumerate(doc.tables[1].columns):
+    for cell in doc.tables[1].column_cells(col_idx):
+        if re.match('\D{2,3} [a-z]{2}',cell.text.strip()):
+            cell.paragraphs[0].clear()
+
+output_filename = 'editad.docx'
+doc.save(output_filename)
+os.system(f'open {os.path.join(os.getcwd(),output_filename)}')
 
 for x in [(col_idx, row_idx, cell.text) for col_idx, column in enumerate(doc.tables[1].columns) for row_idx, cell in enumerate(column.cells)]:
     print(x)
---
+++ python/word_dagplan/weekday.py
@@ -0,0 +1,16 @@
+import datetime
+
+# intDay = datetime.date(year=2000, month=12, day=1).weekday()
+
+def get_weekday(year=None, month=None, day=None):
+    days = ["Mánudagur", "Þriðjudagur", "Miðvikudagur", "Fimmtudagur", "Föstudagur", "Laugardagur", "Sunnudagur"]
+    if not any([year,month,day]):
+        raise ValueError('Need to input date')
+    currentDate = datetime.datetime.today()
+    if not year:
+        if int(month) < currentDate.month:
+            year = currentDate.year + 1
+        else:
+            year = currentDate.year
+    inputDate = datetime.date(year=year,month=int(month),day=int(day))
+    return days[inputDate.weekday()]
