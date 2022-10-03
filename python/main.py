ee4004ae76dbe731012bb723cfc6725af2fa714b
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2022-10-03 08:53:00 +0000

    lol

--- python/main.py
+++ python/main.py
@@ -7,19 +7,24 @@ from numpy import nan
 from pathlib import Path
 import pdfplumber
 import datetime
-
 from collections import defaultdict
-import sys
 import docx
-
 
 output_directory = os.path.join(Path(__file__).parent.resolve().parent.resolve(),'output')
 file = sys.argv[1] if sys.argv[1].endswith('pdf') else '/Users/odinndagur/Downloads/vaktaplans_test/vaktaplan11.09-10.10 lokaútgáfa.pdf'
 filename = os.path.basename(file)
 stripped_filename = os.path.splitext(filename)[0]
-
 print(f'running on file: {file}')
 
+from pdf2image import convert_from_path
+pdfs = convert_from_path(file)
+with pdfplumber.open(file) as pdf:
+    page_1 = pdf.pages[0]
+h,w = page_1.height, page_1.width
+new_h,new_w = pdfs[0].height, pdfs[0].width
+
+# tables = camelot.read_pdf(file,pages='1-end',flavor='lattice',line_scale=20,line_tol=5)
+tables = camelot.read_pdf(file,pages='1-end',flavor='lattice',line_scale=50,line_tol=1)
 
 def get_days(df): #yields generator
     for idx in range(len(df.columns)):
@@ -152,24 +157,10 @@ def get_num_pages(tables):
     else:
         return counts
 
-def get_pdf_size(filepath):
-    with pdfplumber.open(filepath) as pdf:
-        page_1 = pdf.pages[0]
-    return page_1.height, page_1.width
-
-from pdf2image import convert_from_path
-pdfs = convert_from_path(file)
-
-h,w = get_pdf_size(file)
-new_h,new_w = pdfs[0].height, pdfs[0].width
-
 def get_color(img,cell):
     y = new_h - ((cell.y1 + 3)/h * new_h)
     x = ((cell.x1 + cell.x2)/2)/w * new_w
     return img.getpixel((x,y))
-
-# tables = camelot.read_pdf(file,pages='1-end',flavor='lattice',line_scale=20,line_tol=5)
-tables = camelot.read_pdf(file,pages='1-end',flavor='lattice',line_scale=50,line_tol=1)
 
 def get_colors_from_tables():
     colors = set()
@@ -204,39 +195,6 @@ def add_shift_text():
                 cell_color = get_color(pdfs[page_num],cell=cell)
                 if cell_color in colors and (cell.text != '') and re.match('[0-9][0-9]:[0-9][0-9]-[0-9][0-9]:[0-9][0-9]',cell.text):
                     df.iloc[row_num,col_num] += ' ' + colors[cell_color]
-        # df.to_csv('/Users/odinndagur/Desktop/lolz' + str(page_num)+'.csv')
-
-def old_main():
-    parts = []
-    for offset in range(0,len(tables),get_num_pages(tables)):
-        first_page = tables[0+offset].df.iloc[1:,1:].copy()
-        first_page.iloc[0,0] = 'Starfsmaður'
-        second_page = tables[1+offset].df.copy()
-        third_page = tables[2+offset].df.copy()
-
-        first_page.index -= 1
-        first_page.columns -= 1
-        # second_page.index -= 1
-        # third_page.index -= 1
-
-
-        x,y = get_first_date_cell(second_page)
-        first_page.iloc[0,1:] = second_page.iloc[y,x:]
-
-        second_page = second_page.iloc[y+1:,x-1:]
-        second_page.index -= 1 
-        second_page.columns -= 1
-
-        x,y = get_first_date_cell(third_page)
-        third_page = third_page.iloc[y+1:,x-1:]
-        third_page.index -= 1
-        third_page.columns -= 1
-
-        joined = pd.concat([first_page,second_page,third_page],ignore_index = True)
-        joined.iloc[:,:]#.to_csv(os.path.join(output_directory,'out' + str(offset) + '.csv'))
-        parts.append(joined)
-    total = pd.concat(parts,axis=1,ignore_index=True)
-    total.to_csv(os.path.join(output_directory,'output.csv'))
 
 def process_df(table):
     df = table.df.copy()
@@ -251,62 +209,16 @@ def process_df(table):
 add_shift_text()
 
 processed_dfs = [process_df(table) for table in tables]
-
 # concat fyrst
 # concatenated_dfs = [str(offset) + str(idx) for offset in range(0,9,3) for idx in range(3)]
 concatenated_dfs = [pd.concat(processed_dfs[offset:offset+get_num_pages(tables)]) for offset in range(0,tables.n,get_num_pages(tables))]
-
 # síðan join
 output_df = concatenated_dfs[0]
 for df in concatenated_dfs[1:]:
     output_df = output_df.join(df)
 print(f'saving to {output_directory} as {stripped_filename}.csv')
 output_df.to_csv(os.path.join(output_directory,stripped_filename + '.csv'))
-
 print(f'done saving, now making word documents')
 for day in get_days(output_df):
     doc_from_date_day(day)
 print(f'all done')
-
-# pages = [pd.concat([*processed_dfs[offset:offset+get_num_pages(tables)]],axis=0,ignore_index=True) for offset in range(0,tables.n,get_num_pages(tables))]
-# pd.concat([*pages],axis=1,ignore_index=True)
-# parts = []
-# def new_main():
-#     for offset in range(0,len(tables),get_num_pages(tables)): #0, 3, 6
-#         pgs = [process_df(table) for table in tables[offset:offset+get_num_pages(tables)]]
-#         joined = pd.concat([*pgs],axis=0,ignore_index=True)
-#         joined.reset_index()
-#         parts.append(joined)
-#     total = pd.concat([*parts],axis=1,ignore_index=True)
-#     total.to_csv(os.path.join(output_directory,'output.csv'))
-#     print(f'done, file output to {os.path.join(output_directory,"output.csv")}')
-
-
-
-
-# new_main()
-
-
-        # first_page = tables[0+offset].df.copy()
-        # second_page = tables[1+offset].df.copy()
-        # third_page = tables[2+offset].df.copy()
-
-        # x,y = get_first_date_cell(first_page)
-        # first_page.columns = first_page.iloc[y,:]
-        # first_page = first_page.iloc[y+1:,x-1:]
-        # first_page.reset_index(inplace=True, drop=True)
-
-        # x,y = get_first_date_cell(second_page)
-        # second_page.columns = second_page.iloc[y,:]
-        # second_page = second_page.iloc[y+1:,x-1:]
-        # second_page.reset_index(inplace=True, drop=True)
-
-        # x,y = get_first_date_cell(third_page)
-        # third_page.columns = third_page.iloc[y,:]
-        # third_page = third_page.iloc[y+1:,x-1:]
-        # # second_page = second_page.iloc[y+1:,x-1:]
-        # third_page.reset_index(inplace=True, drop=True)
-
-
-        # x,y = get_first_date_cell(third_page)
-        # third_page = third_page.iloc[y+1:,x-1:]
